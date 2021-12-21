a6ef051fa17a0906a6c51fff2b3e8c0a5a7cf85f
milanNavodya <milannavodya70@gmail.com>
2021-12-21 10:51:57 +0530

    New Kotlin class

---
+++ app/src/main/java/com/example/calculator/util/MyClass.kt
@@ -0,0 +1,11 @@
+package com.example.calculator.util
+
+import android.util.Log
+
+const val TAG = "my_tag"
+
+class MyClass {
+    fun saySomething() {
+        Log.d(TAG, "saySomething: Hello!")
+    }
+}
